09d9987a6b895f48004a56a2d12fa362e256c322
Enzo N. Barata <89558747+enzon19@users.noreply.github.com>
2025-10-23 03:54:15 +0000

    feat: isLoading state

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -1,11 +1,20 @@
 <script>
   import { Button } from "flowbite-svelte";
   import { Input } from "flowbite-svelte";
+
+  let { isLoading } = $props();
 </script>
 
-<form class="bg-green- rounded-xl">
-  <Input/>
-  <Button>
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path fill="currentColor" d="M4.4 19.425q-.5.2-.95-.088T3 18.5V14l8-2l-8-2V5.5q0-.55.45-.837t.95-.088l15.4 6.5q.625.275.625.925t-.625.925z"/></svg>
-  </Button>
-</form>
+<Input id="question" name="question" placeholder="Digite uma pergunta..." />
+<Button disabled={isLoading} size="sm" class="cursor-pointer" type="submit">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    ><!-- Icon from Material Symbols by Google - https://github.com/google/material-design-icons/blob/master/LICENSE --><path
+      fill="currentColor"
+      d="M4.4 19.425q-.5.2-.95-.088T3 18.5V14l8-2l-8-2V5.5q0-.55.45-.837t.95-.088l15.4 6.5q.625.275.625.925t-.625.925z"
+    /></svg
+  >
+</Button>
