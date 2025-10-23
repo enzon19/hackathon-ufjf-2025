5f994321811367188edd67972332d30934f95b5e
Enzo N. Barata <89558747+enzon19@users.noreply.github.com>
2025-10-23 07:05:31 +0000

    feat: configure db connection

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,9 +1,20 @@
+import db from "$lib/database";
+
 export const actions = {
   default: async ({ request }) => {
     const data = await request.formData();
     const question = data.get("question");
 
-    // seu processamento aqui
+    try {
+      const result = await db.query({
+        text: "SELECT * FROM bovinos",
+      });
+
+      console.log(result.rows);
+    } catch (e) {
+      console.error(e);
+    }
+
     const answer = {
       from: "agent",
       timestamp: new Date().toISOString(),
