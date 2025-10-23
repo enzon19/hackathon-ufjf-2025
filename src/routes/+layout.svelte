c5295bb5695372f42f83c1905a669513eb0d7bab
Enzo N. Barata <89558747+enzon19@users.noreply.github.com>
2025-10-23 01:28:51 +0000

    feat: header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,21 @@
 <script>
-	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
-	
-	let { children } = $props();
+  import "../app.css";
+  import favicon from "$lib/assets/favicon.svg";
+
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+  <title>AgroChat</title>
+  <link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children?.()}
+<header class="bg-green-700 flex flex-row items-center justify-center p-2">
+  <a href="/">
+    <h1 class="text-white font-bold text-center text-xl">AgroChat</h1>
+  </a>
+</header>
+
+<main class="p-2">
+	{@render children?.()}
+</main>
